37722bd0bad5fd0a03595da0327ae5de8b6ba73d
Fred <fredrik.sandq@gmail.com>
2017-02-15 03:34:44 +0100

    compile rom for next_level2

--- 2nd2006.bat
+++ 2nd2006.bat
@@ -1,2 +1,5 @@
 bin/ca65 2nd2006_simple.a
 bin/ld65 -C nes.cfg -o 2nd2006_simple.nes 2nd2006_simple.o
+
+bin/ca65 2nd2006_next_level2.a
+bin/ld65 -C nes.cfg -o 2nd2006_next_level2.nes 2nd2006_next_level2.o
