199d692299a4ab4accd005dea42d3f83e563a150
Fred <fredrik.sandq@gmail.com>
2017-02-15 23:38:33 +0100

    delete non-working next_level code

--- 2nd2006.bat
+++ 2nd2006.bat
@@ -1,5 +1,5 @@
 bin/ca65 2nd2006_simple.a
 bin/ld65 -C nes.cfg -o roms/2nd2006_simple.nes 2nd2006_simple.o
 
-bin/ca65 2nd2006_next_level2.a
-bin/ld65 -C nes.cfg -o roms/2nd2006_next_level2.nes 2nd2006_next_level2.o
+bin/ca65 2nd2006_next_level.a
+bin/ld65 -C nes.cfg -o roms/2nd2006_next_level.nes 2nd2006_next_level.o
