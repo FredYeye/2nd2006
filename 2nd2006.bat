33d0da666b7263d8996fbc8a1b948db4bd1b73a1
Fred <fredrik.sandq@gmail.com>
2017-02-16 02:10:21 +0100

    cooler adjusting code

--- 2nd2006.bat
+++ 2nd2006.bat
@@ -1,5 +1,5 @@
-bin/ca65 2nd2006_simple.a
-bin/ld65 -C nes.cfg -o roms/2nd2006_simple.nes 2nd2006_simple.o
+../cc65-snapshot-win32/bin/ca65 2nd2006_simple.a
+../cc65-snapshot-win32/bin/ld65 -C nes.cfg -o roms/2nd2006_simple.nes 2nd2006_simple.o
 
-bin/ca65 2nd2006_next_level.a
-bin/ld65 -C nes.cfg -o roms/2nd2006_next_level.nes 2nd2006_next_level.o
+../cc65-snapshot-win32/bin/ca65 2nd2006_next_level.a
+../cc65-snapshot-win32/bin/ld65 -C nes.cfg -o roms/2nd2006_next_level.nes 2nd2006_next_level.o
