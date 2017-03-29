9a79ef4dd0ec4ed27cac2b3c44a55a2438f5d1bf
Fred <fredrik.sandq@gmail.com>
2017-03-29 16:16:46 +0200

    Removed simple version

--- 2nd2006.bat
+++ 2nd2006.bat
@@ -1,5 +1,2 @@
-../cc65-snapshot-win32/bin/ca65 2nd2006_simple.a
-../cc65-snapshot-win32/bin/ld65 -C nes.cfg -o roms/2nd2006_simple.nes 2nd2006_simple.o
-
-../cc65-snapshot-win32/bin/ca65 2nd2006_next_level.a
-../cc65-snapshot-win32/bin/ld65 -C nes.cfg -o roms/2nd2006_next_level.nes 2nd2006_next_level.o
+../cc65-snapshot-win32/bin/ca65 2nd2006.a
+../cc65-snapshot-win32/bin/ld65 -C nes.cfg -o roms/2nd2006.nes 2nd2006.o
